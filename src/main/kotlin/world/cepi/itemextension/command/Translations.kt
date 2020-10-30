438db37d87b0ab42132bafde43b0b91ac3b6f599
LeoDog896 <LeoDog896@hotmail.com>
2020-10-29 21:50:52 -0400

    Make translations const

--- src/main/kotlin/world/cepi/itemextension/command/Translations.kt
+++ src/main/kotlin/world/cepi/itemextension/command/Translations.kt
@@ -1,7 +1,7 @@
 package world.cepi.itemextension.command
 
-val requireFormattedItem = "You are not holding a formatted Item in your hand! Use /item create first."
-val itemIsAir = "You don't have an item in your hand! Please get one first!"
-val requireNonFormattedItem = "This item is already a Cepi item!"
-val usage = "Usage"
-val onlyPlayers = "This command is only available for players!"
+const val requireFormattedItem = "You are not holding a formatted Item in your hand! Use /item create first."
+const val itemIsAir = "You don't have an item in your hand! Please get one first!"
+const val requireNonFormattedItem = "This item is already a Cepi item!"
+const val usage = "Usage"
+const val onlyPlayers = "This command is only available for players!"
