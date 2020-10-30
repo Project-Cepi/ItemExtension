6faf3cbb42ec566bf55868088ba67b22033bb259
LeoDog896 <LeoDog896@hotmail.com>
2020-10-29 21:50:25 -0400

    Allow for creation of items without holding

--- src/main/kotlin/world/cepi/itemextension/command/loaders/actions/CreateAction.kt
+++ src/main/kotlin/world/cepi/itemextension/command/loaders/actions/CreateAction.kt
@@ -5,7 +5,6 @@ import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
 import net.minestom.server.item.Material
 import world.cepi.itemextension.command.checkIsItem
-import world.cepi.itemextension.command.itemIsAir
 import world.cepi.itemextension.command.loaders.ItemCommandLoader
 import world.cepi.itemextension.command.requireNonFormattedItem
 import world.cepi.itemextension.item.Item
@@ -21,15 +20,12 @@ object CreateAction : ItemCommandLoader {
             val player = commandSender as Player
             val itemStack = player.itemInMainHand.clone()
 
-            if (itemStack.material == Material.AIR) {
-                player.sendMessage(itemIsAir)
-                return@addSyntax
-            }
-
             val isCepiItem = checkIsItem(itemStack)
             if (!isCepiItem) {
                 val item = Item()
-                item.addTrait(MaterialTrait(itemStack.material, itemStack.customModelData))
+                if (itemStack.material != Material.AIR) {
+                    item.addTrait(MaterialTrait(itemStack.material, itemStack.customModelData))
+                }
                 player.itemInMainHand = item.renderItem(itemStack.amount)
                 player.sendMessage("Item Created!")
             } else
