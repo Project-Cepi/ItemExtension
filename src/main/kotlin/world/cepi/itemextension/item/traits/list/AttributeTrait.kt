d776e4fa4b467b8cedaa7e137ba3789ca3152e13
LeoDog896 <LeoDog896@hotmail.com>
2020-11-10 10:55:38 -0500

    Update AttributeTrait.kt

--- src/main/kotlin/world/cepi/itemextension/item/traits/list/AttributeTrait.kt
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/AttributeTrait.kt
@@ -1,11 +1,18 @@
 package world.cepi.itemextension.item.traits.list
 
+import net.minestom.server.chat.ChatColor
+import world.cepi.itemextension.command.plus
 import world.cepi.itemextension.item.traits.Trait
 
 interface AttributeTrait : Trait {
 
+    val name: String
+    val value: Int
+
     override fun renderLore(): List<String> {
-        return 
+        return listOf(ChatColor.DARK_GRAY + "▸" + "${ChatColor.BRIGHT_GREEN}$value ${ChatColor.GRAY}$name")
     }
+
+
 
 }
