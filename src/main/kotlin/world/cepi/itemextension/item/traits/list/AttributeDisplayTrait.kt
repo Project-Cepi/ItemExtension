b492188e7c24611d21d2188bd96f0eb616a72790
LeoDog896 <LeoDog896@hotmail.com>
2020-11-10 10:49:45 -0500

    Base attribute trait

---
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/AttributeDisplayTrait.kt
@@ -0,0 +1,15 @@
+package world.cepi.itemextension.item.traits.list
+
+import world.cepi.itemextension.item.traits.Trait
+import world.cepi.itemextension.item.traits.TraitContainer
+
+/** Handles a unified form of stats. Contains traits */
+class AttributeDisplayTrait : TraitContainer<AttributeTrait>, Trait {
+
+    override val traits: MutableList<AttributeTrait> = mutableListOf()
+
+    override fun renderLore(): List<String> {
+        return super.renderLore()
+    }
+
+}
---
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/AttributeTrait.kt
@@ -0,0 +1,11 @@
+package world.cepi.itemextension.item.traits.list
+
+import world.cepi.itemextension.item.traits.Trait
+
+interface AttributeTrait : Trait {
+
+    override fun renderLore(): List<String> {
+        return 
+    }
+
+}
