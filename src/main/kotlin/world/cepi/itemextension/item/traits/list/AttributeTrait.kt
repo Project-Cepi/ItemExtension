2e9ce8898bbd7e468e8d877e906ff2239e2e9665
LeoDog896 <LeoDog896@hotmail.com>
2020-11-13 11:42:13 -0500

    Added TraitList to AttributeTrait

--- src/main/kotlin/world/cepi/itemextension/item/traits/list/AttributeTrait.kt
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/AttributeTrait.kt
@@ -3,8 +3,13 @@ package world.cepi.itemextension.item.traits.list
 import net.minestom.server.chat.ChatColor
 import world.cepi.itemextension.command.plus
 import world.cepi.itemextension.item.traits.Trait
+import world.cepi.itemextension.item.traits.TraitRefrenceList
 
 class AttributeTrait : Trait {
+
+    companion object: TraitRefrenceList(
+
+    )
 
     val name: String = "Example"
     val value: Int = 0
