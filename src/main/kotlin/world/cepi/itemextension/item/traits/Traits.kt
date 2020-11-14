24e42bf6e95044b3f37ba56b2d601c311b0ef524
LeoDog896 <LeoDog896@hotmail.com>
2020-11-13 19:55:04 -0500

    Unify trait naming style

--- src/main/kotlin/world/cepi/itemextension/item/traits/Traits.kt
+++
@@ -1,9 +0,0 @@
-package world.cepi.itemextension.item.traits
-
-import world.cepi.itemextension.item.traits.list.ItemTrait
-import kotlin.reflect.KClass
-
-/** List of traits represented as an enum to contain more data. */
-enum class Traits(val clazz: KClass<out ItemTrait>) {
-
-}
