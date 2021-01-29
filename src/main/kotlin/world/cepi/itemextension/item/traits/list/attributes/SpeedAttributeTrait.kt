4938cecb2f235ee3ce21a36f9a1aad100a3f9a29
LeoDog896 <LeoDog896@hotmail.com>
2021-01-29 08:59:51 -0500

    Revert to no modifiers

--- src/main/kotlin/world/cepi/itemextension/item/traits/list/attributes/SpeedAttributeTrait.kt
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/attributes/SpeedAttributeTrait.kt
@@ -1,21 +1,9 @@
 package world.cepi.itemextension.item.traits.list.attributes
 
-import net.minestom.server.attribute.AttributeOperation
-import net.minestom.server.attribute.Attributes
-import net.minestom.server.item.ItemStack
-import net.minestom.server.item.attribute.AttributeSlot
-import net.minestom.server.item.attribute.ItemAttribute
 import world.cepi.itemextension.item.traits.list.AttributeTrait
-import java.util.*
 
 class SpeedAttributeTrait(override val value: Double): AttributeTrait() {
 
     override val name = "Speed"
-
-    override fun task(item: ItemStack) {
-        AttributeSlot.values().forEach {
-            item.addAttribute(ItemAttribute(UUID.randomUUID(), "e", Attributes.MOVEMENT_SPEED, AttributeOperation.ADDITION, value, it))
-        }
-    }
 
 }
