7cc1fdfe3495ebf29ac2f9241d99ac1a59d84645
LeoDog896 <LeoDog896@hotmail.com>
2021-02-04 11:31:48 -0500

    Dash attack

--- src/main/kotlin/world/cepi/itemextension/item/traits/list/attacks/Attack.kt
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/attacks/Attack.kt
@@ -1,12 +1,18 @@
 package world.cepi.itemextension.item.traits.list.attacks
 
 import net.minestom.server.entity.Player
+import net.minestom.server.sound.Sound
+import net.minestom.server.sound.SoundCategory
 
 enum class Attack(val displayName: String, val action: (Player, Player.Hand) -> Unit = { _, _ -> }) {
 
     STRIKE("Strike"), // does nothing!
-    DASH("Dash", { player, hand ->
-        // TODO dash
+    DASH("Dash", { player, _ ->
+        val direction = player.position.direction.normalize() // Get the general direction the player is looking at
+        direction.multiply(6) // Boosts the direction
+        player.velocity.add(direction)
+//        player.playSound(Sound.ENTITY_ENDER_DRAGON_FLAP, SoundCategory.MASTER, 1f, 1f)
+        player.playSound(Sound.ENTITY_PLAYER_ATTACK_SWEEP, SoundCategory.MASTER, 1f, 1f)
     })
 
 }
