59ce2d69676b089e741575859091da57ac9cc27c
LeoDog896 <LeoDog896@hotmail.com>
2021-02-20 10:25:36 -0500

    Fix event reference

--- src/main/kotlin/world/cepi/itemextension/ItemExtension.kt
+++ src/main/kotlin/world/cepi/itemextension/ItemExtension.kt
@@ -2,14 +2,10 @@ package world.cepi.itemextension
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.extensions.Extension
-import world.cepi.itemextension.events.CombatHandler
-import world.cepi.itemextension.events.DeathHandler
-import world.cepi.itemextension.events.HealthHandler
-import world.cepi.itemextension.events.NoVoidHandler
 import world.cepi.itemextension.command.ClearCommand
 import world.cepi.itemextension.command.GiveCommand
 import world.cepi.itemextension.command.itemcommand.ItemCommand
-import world.cepi.itemextension.events.DisableDropping
+import world.cepi.itemextension.events.*
 
 /** Extension wrapper for Minestom. */
 object ItemExtension : Extension() {
@@ -21,7 +17,7 @@ object ItemExtension : Extension() {
             CombatHandler.register(it)
             DeathHandler.register(it)
             NoVoidHandler.register(it)
-            DisableDropping.load(it)
+            DisableDropping.register(it)
         }
 
         MinecraftServer.getCommandManager().register(ItemCommand())
