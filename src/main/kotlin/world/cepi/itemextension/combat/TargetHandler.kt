6d16a789d04cc0b18aed20a8665141b8a5bdc1d1
LeoDog896 <LeoDog896@hotmail.com>
2021-05-08 12:25:31 -0400

    Reorganization, kotlinifying, and cleanup

--- src/main/kotlin/world/cepi/itemextension/combat/TargetHandler.kt
+++ src/main/kotlin/world/cepi/itemextension/combat/TargetHandler.kt
@@ -1,6 +1,5 @@
 package world.cepi.itemextension.combat
 
-import net.minestom.server.MinecraftServer
 import net.minestom.server.entity.Entity
 import net.minestom.server.entity.LivingEntity
 import net.minestom.server.entity.Metadata
@@ -8,12 +7,12 @@ import net.minestom.server.entity.Player
 import net.minestom.server.item.ItemStack
 import net.minestom.server.network.packet.server.play.EntityMetaDataPacket
 import net.minestom.server.utils.PacketUtils
-import net.minestom.server.utils.Vector
 import net.minestom.server.utils.time.TimeUnit
 import world.cepi.itemextension.item.Item
 import world.cepi.itemextension.item.checkIsItem
 import world.cepi.itemextension.item.module
 import world.cepi.itemextension.item.traits.list.attacks.AttackTrait
+import world.cepi.kstom.Manager
 import world.cepi.kstom.item.get
 import world.cepi.kstom.raycast.HitType
 import world.cepi.kstom.raycast.RayCast.castRay
@@ -28,45 +27,57 @@ object TargetHandler {
     private const val checkTarget = 1L
     private val checkTargetTime = TimeUnit.SECOND
 
-    fun register() {
-        MinecraftServer.getSchedulerManager().buildTask {
-            MinecraftServer.getConnectionManager().onlinePlayers.forEach {
-                val itemInHand = it.getItemInHand(Player.Hand.MAIN)
-                val itemInOffHand = it.getItemInHand(Player.Hand.OFF)
+    fun register() = Manager.scheduler.buildTask {
+        Manager.connection.onlinePlayers.forEach {
+            val itemInHand = it.itemInMainHand
+            val itemInOffHand = it.itemInOffHand
 
-                if((canTarget(itemInHand) || canTarget(itemInOffHand)) && it.instance != null) { // Check if the item supports the targeting system
-                    // TODO: Base max distance upon the item
-                    val result = castRay(it.instance!!, it, Vector(it.position.x, it.position.y + it.eyeHeight, it.position.z), it.position.direction, 100.0, 0.25) // Cast a ray
+            // Check if the item supports the targeting system
+            if ((canTarget(itemInHand) || canTarget(itemInOffHand)) && it.instance != null) {
 
-                    // Check if the ray has hit an entity
-                    if(result.hitType == HitType.ENTITY) {
-                        val target = result.hitEntity!!
-                        if(target is Player) { // Ignore if the entity hit is a player
-                            return@forEach
-                        }
+                // Cast a ray
+                // TODO: Base max distance upon the item
+                val result = castRay(
+                    it.instance!!,
+                    it,
+                    it.position.clone().add(.0, it.eyeHeight, .0).toVector(),
+                    it.position.direction,
+                    100.0,
+                    0.25
+                )
 
-                        if(hasTarget(it)) { // Check if the player already has a target
-                            val currentTarget = targets[it]!!
-                            if(currentTarget != target) { // Switch target and update the entities' glow
-                                targets[it] = target
-                                PacketUtils.sendPacket(it, createMetadataPacket(currentTarget, false))
-                                PacketUtils.sendPacket(it, createMetadataPacket(target, true))
-                            }
-                        } else { // Set the target
+                // Check if the ray has hit an entity
+                if (result.hitType == HitType.ENTITY) {
+
+                    val target = result.hitEntity!!
+
+                    // Ignore if the entity hit is a player
+                    if (target is Player) {
+                        return@forEach
+                    }
+
+                    if (hasTarget(it)) { // Check if the player already has a target
+                        val currentTarget = targets[it]!!
+                        if (currentTarget != target) { // Switch target and update the entities' glow
                             targets[it] = target
+                            PacketUtils.sendPacket(it, createMetadataPacket(currentTarget, false))
                             PacketUtils.sendPacket(it, createMetadataPacket(target, true))
                         }
-                    } else if(hasTarget(it)) { // Remove target if no entity is hit
-                        PacketUtils.sendPacket(it, createMetadataPacket(targets[it]!!, false))
-                        targets.remove(it)
+                    } else { // Set the target
+                        targets[it] = target
+                        PacketUtils.sendPacket(it, createMetadataPacket(target, true))
                     }
-                } else if(hasTarget(it)) { // Remove target if the player swaps to an item which does not have targeting support
+
+                } else if (hasTarget(it)) { // Remove target if no entity is hit
                     PacketUtils.sendPacket(it, createMetadataPacket(targets[it]!!, false))
                     targets.remove(it)
                 }
+            } else if (hasTarget(it)) { // Remove target if the player swaps to an item which does not have targeting support
+                PacketUtils.sendPacket(it, createMetadataPacket(targets[it]!!, false))
+                targets.remove(it)
             }
-        }.repeat(checkTarget, checkTargetTime).schedule()
-    }
+        }
+    }.repeat(checkTarget, checkTargetTime).schedule()
 
     /**
      * Get the [LivingEntity] that a [Player] is targeting
@@ -74,24 +85,23 @@ object TargetHandler {
      * @param player The [Player] which is targeting something
      * @return a [LivingEntity] that is being targeted
      */
-    fun getTarget(player: Player): LivingEntity? {
-        return targets[player]
-    }
+    fun getTarget(player: Player): LivingEntity? =
+        targets[player]
 
     /**
      * Check if a [Player] has a target
      *
      * @param player [Player] to check
      */
-    fun hasTarget(player: Player): Boolean {
-        return targets.containsKey(player)
-    }
+    fun hasTarget(player: Player): Boolean =
+        targets.containsKey(player)
 
     private fun canTarget(item: ItemStack): Boolean {
-        if(checkIsItem(item)) {
+        if (checkIsItem(item)) {
             val cepiItem: Item = item.meta.get(Item.key, module)!!
 
-            return cepiItem.traits.filterIsInstance<AttackTrait>().any { it.attack.requiresTarget } // Check if any attack of an item requires a target
+            // Check if any attack of an item requires a target
+            return cepiItem.traits.filterIsInstance<AttackTrait>().any { it.attack.requiresTarget }
         }
 
         return false
@@ -105,18 +115,19 @@ object TargetHandler {
         clonePacket.entityId = target.entityId
 
         val currentEntry = target.metadataPacket.entries.firstOrNull()
-        val mask = if (currentEntry != null && currentEntry.metaValue.value is Byte) {
-            modifyMask(0x40, glow, currentEntry.metaValue.value as Byte) // 0x40 being the bit for if an entity is glowing
-        } else {
-            modifyMask(0x40, glow) // Assume that no meta is present, thereby the current mask is 0
-        }
 
-        clonePacket.entries = Collections.singleton(Metadata.Entry(0, Metadata.Byte(mask))) as Collection<Metadata.Entry<*>>? // Weird mismatch, seems redundant but for some unknown reason it's required
+        // If no meta is present, the mask should be 0
+        val mask = modifyMask(0x40, glow, currentEntry?.metaValue?.value as? Byte ?: 0)
+
+        // Weird mismatch, seems redundant but for some unknown reason it's required
+        clonePacket.entries = Collections.singleton(Metadata.Entry(0, Metadata.Byte(mask))) as Collection<Metadata.Entry<*>>?
         return clonePacket
     }
 
     private fun modifyMask(bit: Byte, value: Boolean, currentMask: Byte = 0): Byte {
-        val currentValue = (currentMask and bit) == bit // Check if the currentMask "contains" the bit we want to set through bitwise operations.
+
+        // Check if the currentMask "contains" the bit we want to set through bitwise operations.
+        val currentValue = (currentMask and bit) == bit
         if (currentValue == value) {
             return currentMask
         }
