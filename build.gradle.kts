6f93a04d7762a94a6712419516f1d2a2303d93fe
LeoDog896 <LeoDog896@hotmail.com>
2021-06-15 15:49:13 -0400

    Bump KStom

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     compileOnly("com.github.Minestom:Minestom:f2bf27c86e")
 
     // implement KStom
-    compileOnly("com.github.Project-Cepi:KStom:3453ca673b")
+    compileOnly("com.github.Project-Cepi:KStom:813a944c56")
 
     // import kotlinx serialization
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
