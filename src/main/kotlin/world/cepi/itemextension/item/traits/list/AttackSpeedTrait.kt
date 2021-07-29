715e27775da3cdfe355737589427fd167b9f01b4
LeoDog896 <LeoDog896@hotmail.com>
2021-07-29 14:49:00 -0400

    Fix up attack speed

--- src/main/kotlin/world/cepi/itemextension/item/traits/list/AttackSpeedTrait.kt
+++ src/main/kotlin/world/cepi/itemextension/item/traits/list/AttackSpeedTrait.kt
@@ -8,19 +8,22 @@ import net.kyori.adventure.text.format.TextDecoration
 import world.cepi.itemextension.item.Item
 import world.cepi.itemextension.item.traits.ItemTrait
 import world.cepi.itemextension.item.traits.list.stats.StatTrait
+import world.cepi.kstom.serializer.DurationSerializer
+import java.time.Duration
 
 @Serializable
 @SerialName("attack_speed")
 data class AttackSpeedTrait(
         /** The action speed of the trait. */
-        val attackSpeed: Double
+        @Serializable(with = DurationSerializer::class)
+        val attackSpeed: Duration
 ) : ItemTrait() {
 
     override val taskIndex = 1f
 
     override fun renderLore(item: Item): List<Component> {
         return arrayListOf(
-            Component.text("${attackSpeed}s Action Speed", NamedTextColor.GRAY)
+            Component.text("${attackSpeed.toMillis() / 1000}s Action Speed", NamedTextColor.GRAY)
                 .decoration(TextDecoration.ITALIC, false)
         ).also {
             if (item.softHasTrait<StatTrait>()) {
