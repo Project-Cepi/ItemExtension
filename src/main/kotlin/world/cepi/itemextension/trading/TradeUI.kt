90a170d5fe17fcf885a5c773db48d9b091dcbd2e
LeoDog896 <LeoDog896@hotmail.com>
2021-09-26 10:19:57 -0400

    Add display name

--- src/main/kotlin/world/cepi/itemextension/trading/TradeUI.kt
+++ src/main/kotlin/world/cepi/itemextension/trading/TradeUI.kt
@@ -2,6 +2,8 @@ package world.cepi.itemextension.trading
 
 import com.mattworzala.canvas.extra.mask
 import com.mattworzala.canvas.fragment
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.minestom.server.entity.Player
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
@@ -13,15 +15,18 @@ internal object TradeUI {
 
         item(2,
             ItemStack.of(Material.PLAYER_HEAD)
-                .withMeta(PlayerHeadMeta::class.java) {
-                    it.playerSkin(source.skin)
+                .withDisplayName(source.name.color(NamedTextColor.BLUE).decoration(TextDecoration.ITALIC, false))
+                .withMeta(PlayerHeadMeta::class.java) { head ->
+                    source.skin?.let { head.playerSkin(it) }
                 }
+
         )
 
         item(6,
             ItemStack.of(Material.PLAYER_HEAD)
-                .withMeta(PlayerHeadMeta::class.java) {
-                    it.playerSkin(target.skin)
+                .withDisplayName(target.name.color(NamedTextColor.BLUE).decoration(TextDecoration.ITALIC, false))
+                .withMeta(PlayerHeadMeta::class.java) { head ->
+                    target.skin?.let { head.playerSkin(it) }
                 }
         )
 
