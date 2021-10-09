cbbbe4b0439274a71ad079f80b2ef970b7e4aa13
LeoDog896 <LeoDog896@hotmail.com>
2021-10-09 09:46:54 -0400

    Enable dropping if Crews is enabled

--- src/main/kotlin/world/cepi/itemextension/combat/events/DisableDropping.kt
+++ src/main/kotlin/world/cepi/itemextension/combat/events/DisableDropping.kt
@@ -1,10 +1,14 @@
 package world.cepi.itemextension.combat.events
 
 import net.minestom.server.event.item.ItemDropEvent
+import world.cepi.kstom.Manager
 
 object DisableDropping {
 
     fun register(event: ItemDropEvent) = with(event) {
+        // Crews handle item dropping
+        if (Manager.extension.hasExtension("Crews")) return@with
+
         isCancelled = true
     }
 }
