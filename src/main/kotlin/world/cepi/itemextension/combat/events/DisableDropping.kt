635ecac8e7cdea976852b45e98d418c4df658be5
LeoDog896 <LeoDog896@hotmail.com>
2021-10-09 11:25:46 -0400

    hasExtension is case senstive

--- src/main/kotlin/world/cepi/itemextension/combat/events/DisableDropping.kt
+++ src/main/kotlin/world/cepi/itemextension/combat/events/DisableDropping.kt
@@ -7,7 +7,7 @@ object DisableDropping {
 
     fun register(event: ItemDropEvent) = with(event) {
         // Crews handle item dropping
-        if (Manager.extension.hasExtension("Crews")) return@with
+        if (Manager.extension.hasExtension("crews")) return@with
 
         isCancelled = true
     }
