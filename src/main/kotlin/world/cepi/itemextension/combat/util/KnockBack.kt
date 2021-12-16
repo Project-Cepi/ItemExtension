e15659104644f922d5acdd4eb6e2dc8f8c1cca8a
LeoDog896 <LeoDog896@hotmail.com>
2021-12-15 19:45:43 -0500

    Clean up knockback logic

--- src/main/kotlin/world/cepi/itemextension/combat/util/KnockBack.kt
+++ src/main/kotlin/world/cepi/itemextension/combat/util/KnockBack.kt
@@ -5,13 +5,13 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
+const val verticalKnockback = 0.4 * 20
+const val horizontalKnockback = 0.4 * 20
+const val extraHorizontalKnockback = 0.3 * 20
+const val extraVerticalKnockback = 0.1 * 20
+const val limitVerticalKnockback = 0.4 * 20
 
 fun Entity.applyKnockback(attacker: Entity, extra: Float = 0f) {
-    val verticalKnockback = 0.4 * 20
-    val horizontalKnockback = 0.4 * 20
-    val extraHorizontalKnockback = 0.3 * 20
-    val extraVerticalKnockback = 0.1 * 20
-    val limitVerticalKnockback = 0.4 * 20
 
     val distanceX = attacker.position.x() - position.x()
     val distanceY = attacker.position.z() - position.z()
@@ -21,19 +21,14 @@ fun Entity.applyKnockback(attacker: Entity, extra: Float = 0f) {
 
     if (attacker.isSprinting) knockback += 1.25f
 
-    var newVelocity = velocity
+    velocity = velocity
         .withX { x -> (x / 2) - (distanceX / magnitude * horizontalKnockback) }
         .withY { y -> (y / 2) + verticalKnockback }
         .withZ { z -> (z / 2) - (distanceY / magnitude * horizontalKnockback) }
-
-    if (knockback > 0) newVelocity = newVelocity.add(
-        -sin(attacker.position.yaw() * PI / 180.0f) * knockback * extraHorizontalKnockback,
-        extraVerticalKnockback,
-        cos(attacker.position.yaw() * PI / 180.0f) * knockback * extraHorizontalKnockback
-    )
-
-    if (newVelocity.y() > limitVerticalKnockback) newVelocity = newVelocity.withY(limitVerticalKnockback)
-
-    velocity = newVelocity
+        .add(
+            -sin(attacker.position.yaw() * PI / 180.0f) * knockback * extraHorizontalKnockback,
+            if (knockback != 0f) extraVerticalKnockback else 0.0,
+            cos(attacker.position.yaw() * PI / 180.0f) * knockback * extraHorizontalKnockback
+        ).withY { if (it > limitVerticalKnockback) limitVerticalKnockback else it }
 
 }
