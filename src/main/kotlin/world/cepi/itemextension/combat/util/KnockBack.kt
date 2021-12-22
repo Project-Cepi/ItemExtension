5cf49644bc1be806334e0224687891c84fae68ac
LeoDog896 <LeoDog896@hotmail.com>
2021-12-21 21:48:55 -0500

    Take knockback

--- src/main/kotlin/world/cepi/itemextension/combat/util/KnockBack.kt
+++ src/main/kotlin/world/cepi/itemextension/combat/util/KnockBack.kt
@@ -1,34 +1,14 @@
 package world.cepi.itemextension.combat.util
 
 import net.minestom.server.entity.Entity
-import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
-const val verticalKnockback = 0.4 * 20
-const val horizontalKnockback = 0.4 * 20
-const val extraHorizontalKnockback = 0.3 * 20
-const val extraVerticalKnockback = 0.1 * 20
-const val limitVerticalKnockback = 0.4 * 20
-
 fun Entity.applyKnockback(attacker: Entity, extra: Float = 0f) {
 
-    val distanceX = attacker.position.x() - position.x()
-    val distanceY = attacker.position.z() - position.z()
-
-    val magnitude = attacker.position.distance(position)
-    var knockback = extra
-
-    if (attacker.isSprinting) knockback += 1.25f
-
-    velocity = velocity
-        .withX { x -> (x / 2) - (distanceX / magnitude * horizontalKnockback) }
-        .withY { y -> (y / 2) + verticalKnockback }
-        .withZ { z -> (z / 2) - (distanceY / magnitude * horizontalKnockback) }
-        .add(
-            -sin(attacker.position.yaw() * PI / 180.0f) * knockback * extraHorizontalKnockback,
-            if (knockback != 0f) extraVerticalKnockback else 0.0,
-            cos(attacker.position.yaw() * PI / 180.0f) * knockback * extraHorizontalKnockback
-        ).withY { if (it > limitVerticalKnockback) limitVerticalKnockback else it }
+    takeKnockback(1 + extra,
+        sin(attacker.position.yaw() * 0.017453292F).toDouble(),
+        -cos(attacker.position.yaw() * 0.017453292F).toDouble()
+    );
 
 }
