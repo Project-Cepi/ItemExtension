7966bf2fd817d7f9e0cfd4ac9952c75436c1b79a
LeoDog896 <LeoDog896@gmail.com>
2022-01-20 16:46:23 -0500

    Allow for multiple give arguments

--- src/main/kotlin/world/cepi/itemextension/command/GiveCommand.kt
+++ src/main/kotlin/world/cepi/itemextension/command/GiveCommand.kt
@@ -2,6 +2,7 @@ package world.cepi.itemextension.command
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.minestom.server.command.builder.CommandContext
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
 import world.cepi.itemextension.command.itemcommand.loaders.ConditionLoader
@@ -16,35 +17,43 @@ object GiveCommand : Kommand({
     val itemArg = ArgumentType.ItemStack("item")
     val amountArg = ArgumentType.Integer("amount").min(1).max(127)
 
+    val items = ArgumentType.Loop("loop", ArgumentType.Group("itemGroup", itemArg, amountArg))
+
     amountArg.defaultValue = Supplier { 1 }
 
     val selector = ArgumentType.Entity("players").onlyPlayers(true)
 
-    syntax(selector, itemArg, amountArg) {
-        val item = context.get(itemArg).withAmount(context.get(amountArg))
+    syntax(selector, items) {
 
         val targets = context.get(selector).find(player.instance!!, player).map { it as Player }
 
-        targets.forEach {
-            (it as? Player)?.inventory?.addItemStack(context.get(itemArg))
+        targets.forEach { target ->
+            (!items).forEach {
+                val item = it.get(itemArg).withAmount(it.get(amountArg))
+                (target as? Player)?.inventory?.addItemStack(item)
+            }
         }
+
+        val itemNames = (!items).map { context ->
+            Component.text(context.get(amountArg), NamedTextColor.BLUE)
+                .append(Component.space())
+                .append(context.get(itemArg).displayName ?: Component.text(
+                    context.get(itemArg).material
+                        .namespace().path
+                        .lowercase()
+                        .split("_")
+                        .joinToString(" ") { it.replaceFirstChar { it.uppercase() } }
+                ) as Component).hoverEvent(context.get(itemArg).asHoverEvent())
+        }.reduce { acc, component -> acc.append(Component.text(", ", NamedTextColor.GRAY)).append(component) }
 
         // TODO plural translation
         sender.sendFormattedTranslatableMessage(
             "item", "give",
-            Component.text(context.get(amountArg), NamedTextColor.BLUE),
-            (item.displayName
-                ?: Component.text(
-                    item.material
-                        .name()
-                        .lowercase()
-                        .split("_")
-                        .joinToString(" ") { it.replaceFirstChar { it.uppercase() } }
-                ))
-                .hoverEvent(item.asHoverEvent()),
+            (itemNames),
             (
                     if (targets.size == 1) Component.text(targets[0].username)
-                    else Component.text("${targets.size} Players")).color(NamedTextColor.BLUE)
+                    else Component.text("${targets.size} Players")
+            ).color(NamedTextColor.BLUE)
         )
 
     }
